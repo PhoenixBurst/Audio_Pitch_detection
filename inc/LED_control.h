1d968fac98a0430a32e54c4f3c3184b75afd577e
Sören Schreiber <phoenixburst@arcor.de>
2016-03-24 18:47:40 +0000

    Added more comments to the source code and header files
    -comments for LED control.c/.h
    -comments for audio_pitch_detection.c

--- inc/LED_control.h
+++ inc/LED_control.h
@@ -24,12 +24,13 @@
 *
 * Version:
 *		1.0		15/03/2016
-*		1.1		16/03/2016
+*		1.1		16/03/2016		--		working implementations in the source file, tested said functions
+*		1.1		24/03/2016		--		added comments to header file
 *
 */
 
 #ifndef __LED_CONTROL__
-#define __LED_CONTROL__
+#define __LED_CONTROL__		//define __LED_CONTROL__
 
 #define STATE_READY 0 //ready state
 #define STATE_RUN 1 //run state
@@ -44,12 +45,12 @@
 #define clock_frequency 40e6 // standart clock frequency
 #define timeout 0.3 //wait time for the timer 
 
-int displayState(int state); //switch between different running states
+int displayState(int state); //switch between different running states[0=READY, 1=RUN, 2=ERROR]
 
 void readyState(); //display ready state on leds
 
 void errorState(); //display error state on leds
 
-void displayLED(int led_red, int led_yellow, int led_green); //display user selected leds
+void displayLED(int led_red, int led_yellow, int led_green); //display user selected leds[1=ON, 0=OFF]
 
 #endif
--- src/LED_control.c
+++ src/LED_control.c
@@ -4,14 +4,16 @@
 *		LED Control function for different programm states
 *
 * Inputs:
-*		State of the programm, LEDs
+*		State of the programm, LEDs			int, define states
 *
 * Outputs:
 *		LEDs output
 *
 * Functions:
-*		void displayState(int state)
+*		int displayState(int state)
 *		void displayLED(int led_red, int led_yellow, int led_green)
+*		void readyState()
+*		void errorState()
 *
 * Notes:
 *		none
@@ -21,7 +23,8 @@
 *
 * Version:
 *		1.0		15/03/2016
-*		1.1		16/03/2016
+*		1.1		16/03/2016		--		working implementations for all functions + testing of said implementations
+*		1.1		24/03/2016		--		added comments to source code
 *
 */
 #include <board\h\sask.h>
@@ -29,141 +32,140 @@
 
 #include "..\inc\LED_control.h"
 
-int currentState;
+int currentState; //Variable containing the programs current state -> returned to calling program code for further use
 
-float instruction_clock_frequency, instruction_cycle_time;
-float wait_time;
+float instruction_clock_frequency, instruction_cycle_time;	//variables needed for timer functions -> types taken from the needs of the timer functions
 
 
-int displayState(int state)
+int displayState(int state)	//hub function for state selection of the program
 {
 	switch(state)
 	{
 		case 0:
-				currentState=0;
-				readyState();
+				currentState=0;	//set currentState to 0
+				readyState();	//display ready state on the LEDs
 				break;
 		case 1:
-				currentState=1;
+				currentState=1;	//set current state to 1
 				break;
 		default:
-				currentState=2;
-				errorState();
+				currentState=2;	//set current state to 2
+				errorState();	//display error state on the LEDs
 	}
 
-	return currentState;
+	return currentState;	//return current state to the calling program code
 }
 
-void displayLED(int led_red, int led_yellow, int led_green)
+void displayLED(int led_red, int led_yellow, int led_green)	//function for selective LED control
 {
-	if(led_red==1)
+	if(led_red==1)	
 	{
-		RED_LED=SASK_LED_ON;
+		RED_LED=SASK_LED_ON;	//turn on red LED
 	}
 	else if(led_red==0)
 	{
-		RED_LED=SASK_LED_OFF;
+		RED_LED=SASK_LED_OFF;	//turn off red LED
 	}
 
 	if(led_yellow==1)
 	{
-		YELLOW_LED=SASK_LED_ON;
+		YELLOW_LED=SASK_LED_ON;	//turn on yellow LED
 	}
 	else if(led_yellow==0)
 	{
-		YELLOW_LED=SASK_LED_OFF;
+		YELLOW_LED=SASK_LED_OFF;//turn off yellow LED
 	}
 
 	if(led_green==1)
 	{
-		GREEN_LED=SASK_LED_ON;
+		GREEN_LED=SASK_LED_ON;	//turn on green LED
 	}
 	else if(led_green==0)
 	{
-		GREEN_LED=SASK_LED_OFF;
+		GREEN_LED=SASK_LED_OFF;	//turn off green LED
 	}
 }
 
-void readyState()
+void readyState()	//function to display ready state using the LEDs
 {
-	instruction_clock_frequency = clock_frequency;
-	instruction_cycle_time = 1.0 / instruction_clock_frequency;
+	instruction_clock_frequency = clock_frequency;				//set up variable needed for fo the timer function
+	instruction_cycle_time = 1.0 / instruction_clock_frequency;	//set up variable needed for fo the timer function
 
-	ex_timer_init( instruction_cycle_time );
+	ex_timer_init( instruction_cycle_time );			//init timer 
 
-	int led_selector=0;
-	while(currentState==0)
+	int led_selector=0;	//determins which LED will be turned on in the current cycle
+	while(currentState==0)	//while program is in ready state
 	{
-		if((SWITCH_S1==0) && (SWITCH_S2==1)) //bypassing checkswitch function
+		if((SWITCH_S1==0) && (SWITCH_S2==1))	//checking if Switch 1 is pressed and switch 2 is unpressed (direct read of the buttons to bypass a debouncer problem)
 		{
-			currentState=1;
+			currentState=1;	//set current state to 1
 		}
 		else
 		{
 			switch(led_selector)
 			{
-				case 0:
+				case 0:			//turn on red LED, turn off yellow and green LEDs
 					RED_LED=SASK_LED_ON;
 					YELLOW_LED=SASK_LED_OFF;
 					GREEN_LED=SASK_LED_OFF;
 					led_selector=led_selector+1;
 					break;
-				case 1:
+				case 1:			//turn on yellow LED, turn off red and green LEDs
 					RED_LED=SASK_LED_OFF;
 					YELLOW_LED=SASK_LED_ON;
 					GREEN_LED=SASK_LED_OFF;
 					led_selector=led_selector+1;
 					break;
-				case 2:
+				case 2:			//turn on green LED, turn off red and yellow LEDs
 					RED_LED=SASK_LED_OFF;
 					YELLOW_LED=SASK_LED_OFF;
 					GREEN_LED=SASK_LED_ON;
 					led_selector=0;
 					break;
-				default:
+				default:		//if an error occurse set program into error state
 					errorState();
 			}
 		
-			ex_timer_wait(timeout);
+			ex_timer_wait(timeout); //wait for the time[in seconds] set in timeout(defined in header file)
 		}
 	}
 }
 
-void errorState()
+void errorState()		//display error state on the LEDs
 {
-	instruction_clock_frequency = clock_frequency;
-	instruction_cycle_time = 1.0 / instruction_clock_frequency;
+	instruction_clock_frequency = clock_frequency;					//set up variable needed for fo the timer function
+	instruction_cycle_time = 1.0 / instruction_clock_frequency;		//set up variable needed for fo the timer function
 
-	ex_timer_init( instruction_cycle_time );
+	ex_timer_init( instruction_cycle_time );		//init timer
 
-	int current_led_state=FALSE;
+	int current_led_state=FALSE; 	//set LED status to OFF
 
 	while(1)
 	{
-		if((SWITCH_S1==0) && (SWITCH_S2==0))
+		if((SWITCH_S1==0) && (SWITCH_S2==0))	//check if switch 1 and 2 are pressed
 		{
-			current_led_state=FALSE;
-			currentState=0;
+			current_led_state=FALSE;	//set LED status to OFF
+			currentState=0;			//set current state to ready
 			break;
 		}
-		else if(current_led_state==FALSE)
+		else if(current_led_state==FALSE) //if LEDs are off turn them on
 		{
 			RED_LED=SASK_LED_ON;
 			YELLOW_LED=SASK_LED_ON;
 			GREEN_LED=SASK_LED_ON;
 
-			current_led_state=TRUE;
+			current_led_state=TRUE;		//invert current LED status
 		}
-		else if(current_led_state==TRUE)
+		else if(current_led_state==TRUE) //if LEDs are on turn them off
 		{
 
 			RED_LED=SASK_LED_OFF;
 			YELLOW_LED=SASK_LED_OFF;
 			GREEN_LED=SASK_LED_OFF;
 
-			current_led_state=FALSE;
+			current_led_state=FALSE;	//invert current LED status
 		}
 
-		ex_timer_wait(timeout);
+		ex_timer_wait(timeout);	//wait for the time[in seconds] set in timeout(defined in header file)
 	}
 }
